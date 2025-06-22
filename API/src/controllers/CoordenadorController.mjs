fd3ccf30e9714a6b78b31b2f327704bd95594290
Lucas Paulo <lp152659@gmail.com>
2025-06-21 21:57:52 -0300

    Modificação de lógica de negócios (coordenador) e atribuições de funcionalidades de gerência ao coordenador

--- API/src/controllers/CoordenadorController.mjs
+++ API/src/controllers/CoordenadorController.mjs
@@ -5,6 +5,8 @@ import Turma from "../models/Turma.mjs";
 import Aluno from "../models/Aluno.mjs";
 
 
+//CADASTRO DE PROFESSORES, ALUNOS, TURMAS E DISCIPLINAS ()
+
 //Cadastrar as turmas
 export const registerClasses = async (req, res) => {
     const {
@@ -29,9 +31,7 @@ export const registerClasses = async (req, res) => {
             nome,
             turno,
             anoLetivo,
-            alunos: [],
-            professores: [],
-            disciplinas: []
+
         });
 
         res.status(201).json({
@@ -47,13 +47,12 @@ export const registerClasses = async (req, res) => {
 }
 
 
-//Cadastrando alunos em suas determinadas turmas
+//Cadastrando alunos
 export const registerStudent = async (req, res) => {
     const {
         nome,
         matricula,
         dataNasc,
-        turma,
 
     } = req.body;
 
@@ -66,51 +65,33 @@ export const registerStudent = async (req, res) => {
             return res.status(422).json({ errors: ["Estudante já matriculado!"] });
         }
 
-        //Buscando turma no banco
-        const turmaExistente = await Turma.findById(turma);
-
-        //Se não existir emite o erro
-        if (!turmaExistente) {
-            return res.status(422).json({ errors: ["Turma não encontrada!"] });
-        }
-
-        //Validando turnos
-        const turnosValidos = ["manhã", "tarde"];
-
-        if (!turnosValidos.includes(turmaExistente.turno)) {
-            return res.status(422).json({ errors: ["Turno inválido!"] });
-        }
-
         //Criando novo estudante
         const newAluno = await Aluno.create({
             nome,
             matricula,
             dataNasc,
-            turma
+
         });
 
-
-        //Atribuindo estudante a uma turma
-        await Turma.findByIdAndUpdate(turma, {
-            $push: { alunos: newAluno._id }
+        res.status(201).json({ 
+            msg: "Aluno matriculado com sucesso!",
+            aluno: newAluno
         });
-
-        res.status(201).json({ msg: "Aluno matriculado com sucesso!" });
 
     }
     catch (error) {
         res.status(500).json({ errors: ["Erro interno do servidor!"] });
+        console.log(error)
     }
 }
 
-//Cadastrar os professores nas determinadas turmas
+
+//Cadastrar os professores
 export const registerTeacher = async (req, res) => {
     const {
         user,
         matricula,
         formacaoAcademica,
-        disciplinas,
-        turmas
 
     } = req.body;
 
@@ -130,26 +111,7 @@ export const registerTeacher = async (req, res) => {
             matricula,
             nome: user.nome,
             formacaoAcademica,
-            disciplinas: [],
-            turmas: []
         });
-
-        //Inserindo professor no esquema de disciplina
-        if (disciplinas?.length) {
-            await Disciplina.updateMany(
-                { _id: { $in: disciplinas } },
-                { $addToSet: { professores: newProfessor._id } }
-            );
-        }
-
-        // Adiciona o professor nas turmas
-    if (turmas?.length) {
-      await Turma.updateMany(
-        { _id: { $in: turmas } },
-        { $addToSet: { professores: newProfessor._id } }
-      );
-    }
-
 
         res.status(201).json({
             msg: "Professor cadastrado com sucesso!",
@@ -159,5 +121,232 @@ export const registerTeacher = async (req, res) => {
     }
     catch (error) {
         res.status(500).json({ errors: ["Erro interno do servidor!"] });
+        console.log(error);
     }
 }
+
+//Cadastrar disciplinas
+export const registerDisciplines = async (req, res) => {
+    const { 
+        nome, 
+        descrição, 
+        cargaHoraria, 
+    } = req.body;
+
+    try {
+
+        //Buscar disciplina por nome
+        const disciplina = await Disciplina.findOne({ nome });
+
+
+        //Não permite salvar duas disciplinas com o mesmo nome
+        if (disciplina) {
+            return res.status(404).json({ errors: ["Disciplina já existe!"] });
+        }
+
+        //Salvando disciplina
+        const newDiscipline = await Disciplina.create({
+            nome,
+            descrição,
+            cargaHoraria,
+        });
+
+        res.status(201).json({
+            msg: "Disciplina cadastrada com sucesso!",
+            disciplina: newDiscipline
+        });
+
+    }
+    catch (error) {
+        res.status(500).json({ errors: ["Erro interno do servidor!"] });
+        console.log(error);
+    }
+}
+
+//PREPARANDO OS RELACIONAMENTOS (CADASTROS ENTRE OS ESQUEMAS)
+
+//Atribuindo turma a um professor
+
+export const classToTeacher = async (req, res) => {
+    const { teacherId, classId } = req.body;
+
+    try{
+        
+        //Buscando e validando turma
+        const classroom = await Turma.findById(classId);
+
+        if(!classroom){
+            return res.status(404).json({ errors: ["Turma não encontrada!"] });
+        } 
+
+        //Buscando e validando Professor
+        const teacher = await Professor.findById(teacherId);
+
+        if(!teacher){
+            return res.status(404).json({ errors: ["Professor não encontrado"] });
+        }
+
+        //Verificando se o professor já está na turma
+        const isInClassroom = classroom.professores.includes(teacherId);
+
+        if(isInClassroom){
+            return res.status(422).json({ errors: ["O professor já está na turma!"] });
+        }
+
+        //Salvando professor na turma caso não esteja e inserindo id da turma no esquema do professor
+        classroom.professores.push(teacherId);
+
+        //Salvando turma
+        await classroom.save();
+
+        //Inserindo turma no esquema de professor
+        teacher.Turmas.push(classId);
+
+        //Salvando professor
+        await teacher.save();
+
+        res.status(201).json({
+            msg: "Turma atribuída ao professor com sucesso!"
+        });
+
+    }
+    catch(error){
+        res.status(500).json({ errors: ["Erro interno do servidor!"] });
+        console.log(error);
+    }
+}
+
+//Adicionando disciplinas ao professor
+
+export const disciplineToTeacher = async (req, res) => {
+    const { disciplineId, teacherId } = req.body;
+
+    try{
+
+        //Buscando disciplina
+        const discipline = await Disciplina.findById(disciplineId);
+
+        if(!discipline){
+            return res.status(404).json({ errors: ["Disciplina não encontrada!"] });
+        }
+
+        //Buscando professor
+        const teacher = await Professor.findById(teacherId);
+
+        if(!teacher){
+            return res.status(404).json({ errors: ["Professor não encontrado!"] });
+        }
+
+        //Validando se o professor já está registrado na disciplina
+        if(discipline.professores.includes(teacherId)){
+            return res.status(422).json({ errors: ["O professor já está registrado na disciplina!"] });
+        }
+
+        //Salvando professor na disciplina
+        discipline.professores.push(teacherId);
+
+        await discipline.save();
+
+        //Salvando disciplina no esquema do professor
+        teacher.disciplinas.push(disciplineId);
+
+        await teacher.save();
+
+        res.status(201).json({
+            msg: "Disciplina atribuída ao professor com sucesso!"
+        })
+
+    }
+    catch(error){
+        res.status(500).json({ errors: ["Erro interno do servidor!"] });
+        console.log(error);
+    }
+}
+
+//Adicionando disciplina a turma
+
+export const disciplineToClass = async (req, res) => {
+    const { disciplineId, classId } = req.body;
+
+    try{
+
+        //Buscando disciplina
+        const discipline = await Disciplina.findById(disciplineId);
+
+        if(!discipline){
+            return res.status(404).json({ errors: ["Disciplina não encontrada!"] });
+        }
+
+        //Buscando Turma
+        const classroom = await Turma.findById(classId);
+
+        if(!classroom){
+            return res.status(404).json({ errors: ["Turma não encontrada!"] });
+        }
+
+        //Verificando se disciplina não já está em turma
+        if(classroom.disciplinas.includes(disciplineId)){
+            return res.status(404).json({ errors: ["A disciplina já está registrada na turma!"] });
+        }
+
+        classroom.disciplinas.push(disciplineId);
+
+        await classroom.save();
+
+        res.status(201).json({
+            msg: "Disciplina registrada à turma com sucesso!"
+        })
+    }
+    catch(error){
+        res.status(500).json({ errors: ["Erro interno do servidor!"] });
+        console.log(error);
+    }
+}
+
+//Adicionando aluno á turma
+
+export const studentToClass = async (req, res) => {
+    const { studentId, classId } = req.body;
+
+    try{
+
+        //Buscando aluno
+        const student = await Aluno.findById(studentId);
+
+        if(!student){
+            return res.status(404).json({ errors: ["Aluno(a) não encontrado(a)!"] });
+        }
+
+        //Buscando turma
+        const classroom = await Turma.findById(classId);
+
+        if(!classroom){
+            return res.status(404).json({ errors: ["Turma não encontrada!"] });
+        }
+
+        //Verificando se aluno não já está na turma
+        if(classroom.alunos.includes(studentId)){
+            return res.status(442).json({ errors: ["Aluno(a) já está na turma!"] });
+        }
+
+        //Salvando 
+        classroom.alunos.push(studentId);
+
+        await classroom.save();
+
+        student.turma = classId;
+
+        await student.save();
+
+        res.status(201).json({
+            msg: "Aluno salvo na turma!"
+        })
+
+    }
+    catch(error){
+        res.status(500).json({ errors: ["Erro interno do servidor!"] });
+        console.log(error);
+    }
+}
+
+
--- API/src/routes/coordenadorRoutes.mjs
+++ API/src/routes/coordenadorRoutes.mjs
@@ -6,16 +6,28 @@ import { authGuard } from "../middlewares/authGuard.mjs";
 import { authorizeRole } from "../middlewares/authorizeRole.mjs";
 
 //Rotas
-import { registerClasses } from "../controllers/CoordenadorController.mjs";
-import { registerStudent } from "../controllers/CoordenadorController.mjs";
-import { registerTeacher } from "../controllers/CoordenadorController.mjs";
+import { 
+    registerClasses ,
+    registerStudent,
+    registerTeacher,
+    registerDisciplines,
+    classToTeacher,
+    disciplineToTeacher,
+    disciplineToClass,
+    studentToClass
+
+} from "../controllers/CoordenadorController.mjs";
+
 
 //Configurações de rotas
 coordenadorRouter.post('/register-classes', authGuard, authorizeRole("Coordenador"), registerClasses);
-
 coordenadorRouter.post('/register-students', authGuard, authorizeRole("Coordenador"), registerStudent);
-
 coordenadorRouter.post('/register-teacher', authGuard, authorizeRole("Coordenador"), registerTeacher);
+coordenadorRouter.post('/register-discipline', authGuard, authorizeRole("Coordenador"), registerDisciplines);
+coordenadorRouter.put('/classToTeacher', authGuard, authorizeRole("Coordenador"), classToTeacher);
+coordenadorRouter.put('/disciplineToTeacher', authGuard, authorizeRole("Coordenador"), disciplineToTeacher);
+coordenadorRouter.put('/disciplineToClass', authGuard, authorizeRole("Coordenador"), disciplineToClass);
+coordenadorRouter.put('/studentToClass', authGuard, authorizeRole("Coordenador"), studentToClass);
 
 
 export default coordenadorRouter;
