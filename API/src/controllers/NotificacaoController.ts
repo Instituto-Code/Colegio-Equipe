4dfeee326df9220567feeaf3590a1eb518495610
Luan Tomaz <luantomazz9000@gmail.com>
2025-10-08 14:15:56 -0300

    feat: Adicionando funcionalidade de edição da Notificação

--- API/src/controllers/NotificacaoController.ts
+++ API/src/controllers/NotificacaoController.ts
@@ -27,19 +27,42 @@ export const createNote = async (req: CustomRequest, res: Response) => {
     }
 };
 
- export const deleteNote = async (req: CustomRequest, res:Response) => {
+ export const deleteNote = async (req: CustomRequest, res: Response) => {
     try {
-        const { id } = req.params;
+        const { id } = req.body;
 
-        const note = await Notificacoes.findByPK(id);
+        const note = await Notificacoes.findByIdAndDelete(id);
         if (!note) {
             return res.status(404).json({error: "Notificação não encontrada!" });
         }
-        await note.destroy();
-        res.status().json({message: "Notificação excluída com sucesso!"});
+        res.status(200).json({message: "Notificação excluída com sucesso!"});
     }
     catch (error) {
         res.status(500).json({error: "Erro interno no servidor"});
         console.error(error);
     }
+ }
+
+ export const updateNote = async (req: CustomRequest, res:Response) => {
+    try {
+        const { id, conteudo, tipo, } = req.body;
+
+        const note = await Notificacoes.findById(id);
+        if(!note) {
+            return res.status(404).json({error: "Notificação não encontrada!"});
+        }
+
+        if (conteudo) note.conteudo = conteudo;
+        if (tipo) note.tipo = tipo;
+
+        await note.save();
+
+        res.status(200).json({message: "Notificação atualizada com sucesso", note});
+    }
+
+    catch (error) {
+        res.status(500).json({error: "Erro interno no servidor"});
+        console.error(error);
+    }
+
  }
--- API/src/routes/NotificacaoRoutes.ts
+++ API/src/routes/NotificacaoRoutes.ts
@@ -2,11 +2,12 @@ import express from "express";
 const noteRouter = express.Router();
 
 
-import { createNote } from "../controllers/NotificacaoController.js";
+import { createNote, deleteNote, updateNote } from "../controllers/NotificacaoController.js";
 import { authGuard } from "../middlewares/authGuard.js";
 import { authorizeRole } from "../middlewares/authorizeRole.js";
 
 noteRouter.post("/create-note", authGuard, authorizeRole('coordenador'), createNote);
-
+noteRouter.delete("/delete-note", authGuard, authorizeRole('coordenador'), deleteNote);
+noteRouter.patch("/update-note", authGuard, authorizeRole('coordenador'), updateNote);
 
 export default noteRouter;
