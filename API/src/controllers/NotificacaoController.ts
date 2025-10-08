b3a3fee0e537a4682fa0ab34e1cdfb71394b9513
Luan Tomaz <luantomazz9000@gmail.com>
2025-10-07 21:32:14 -0300

    feat: Adicionando funcionalidade de deletar Notificação

--- API/src/controllers/NotificacaoController.ts
+++ API/src/controllers/NotificacaoController.ts
@@ -26,3 +26,20 @@ export const createNote = async (req: CustomRequest, res: Response) => {
         console.error(error);
     }
 };
+
+ export const deleteNote = async (req: CustomRequest, res:Response) => {
+    try {
+        const { id } = req.params;
+
+        const note = await Notificacoes.findByPK(id);
+        if (!note) {
+            return res.status(404).json({error: "Notificação não encontrada!" });
+        }
+        await note.destroy();
+        res.status().json({message: "Notificação excluída com sucesso!"});
+    }
+    catch (error) {
+        res.status(500).json({error: "Erro interno no servidor"});
+        console.error(error);
+    }
+ }
